570457f243e934e937cb16773209d365b1cff58f
Tim Kuhlman <tim.kuhlman@hp.com>
2014-03-17 16:25:41 -0600

    Set from address to something acceptable to the hp smtp server.

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'hpcs-mon@hp.com'
 license          'All rights reserved'
 description      'Installs/Configures mon_notification'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
