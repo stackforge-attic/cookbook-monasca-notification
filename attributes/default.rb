5afeaa06861918e85a26c67744766c92ec35a589
Craig Bryant <craig.bryant@hp.com>
2014-08-25 19:34:44 -0600

    Create a monasca group
    All monasca users will belong to the monasca group so
    /var/log/monasca can be writable for all monasca users so there can
    be the one monasca log directory
    
    There will no longer be a monasca-notification group
    
    The code adding this group will be added to other cookbooks so the group
    will be created correctly no matter which cookbook runs first
    
    Change-Id: Ic47701fa24d33a8d8d713697901f13c09790c87d

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 #
 default[:monasca_notification][:user] = 'monasca-notification'
-default[:monasca_notification][:group] = 'monasca-notification'
+default[:monasca_notification][:group] = 'monasca'
 default[:monasca_notification][:conf_dir] = '/etc/monasca'
 default[:monasca_notification][:log_dir] = '/var/log/monasca'
 default[:monasca_notification][:data_bag] = 'monasca_notification'
