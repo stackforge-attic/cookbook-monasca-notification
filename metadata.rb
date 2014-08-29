aa66a3c20a7745f86f67a98ef3bcabdb9ecd7b01
Tim Kuhlman <tim.kuhlman@hp.com>
2014-08-29 13:39:33 -0600

    If zookeeper is not accessible respawn is too fast for the default settings.
    Change-Id: I8b3d1e7fa671b47e42fa449c63255b141b0ab207

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ maintainer_email 'hpcs-mon@hp.com'
 license          'All rights reserved'
 description      'Installs/Configures monasca_notification'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.19'
+version          '0.1.20'
 depends          'python'
